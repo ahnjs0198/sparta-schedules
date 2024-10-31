bab8140496b411971066ac3611c882e9dd1989db
ahnjs0198 <ahnjs0198@gmail.com>
2024-10-31 19:54:17 +0900

    Docs: SQL 작성

--- schedule.sql
+++ schedule.sql
@@ -11,4 +11,22 @@ CREATE TABLE schedules (
 SELECT id, todo, name, updatedAt
 FROM schedules;
 
+-- 선택 일정 조회 query
+SELECT id, todo, name, updatedAt
+FROM schedules
+WHERE id = 1;
+
+-- 일정 생성 query
+INSERT INTO schedules (id, todo, name, updatedAt)
+VALUES (1, '장보기', '김르탄','1234','2024-10-31 19:51:00');
+
+-- 일정 수정 query
+UPDATE schedules
+SET todo = '식사하기'
+WHERE id = 1;
+
+-- 일정 삭제 query
+DELETE FROM schedules
+WHERE id = 1;
+
 
